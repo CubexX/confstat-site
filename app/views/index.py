3c00124adccbe247d1fa7174377fe9706cf022b1
Alexey Miroshnichenko <CubexX@404.pm>
2018-01-19 19:21:03 +0000

    Some fixes

--- app/views/index.py
+++ app/views/index.py
@@ -1,26 +1,25 @@
 # -*- coding: utf-8 -*-
 __author__ = 'CubexX'
 
-from app import app
+from app import app, cache
 from app.models import User, Chat, ChatStat
 from flask import render_template
-from datetime import datetime
 
 
 @app.route('/')
 def index():
-    users_count = User.all().count()
-    chats_count = Chat.all().count()
-    messages_count = 0
+    stats = cache.get('web_stats')
 
-    t = datetime.today()
-    today_ts = datetime(t.year, t.month, t.day, hour=00).timestamp()
-
-    for chat in ChatStat.where('last_time', '>', today_ts).get().all():
-        messages_count += chat.msg_count
+    if not stats:
+        stats = {
+            'users_count': User.all().count(),
+            'chats_count': Chat.all().count(),
+            'messages_count': sum(chat.msg_count for chat in ChatStat.all())
+        }
+        cache.set('web_stats', stats, 300)
 
     return render_template('index.html',
                            page_title='Confstat',
-                           users_count=users_count,
-                           chats_count=chats_count,
-                           messgaes_count=messages_count)
+                           users_count=stats['users_count'],
+                           chats_count=stats['chats_count'],
+                           messgaes_count=stats['messages_count'])
